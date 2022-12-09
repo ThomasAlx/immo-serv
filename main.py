686ca0a1f35b1db1c5cc0481459d82582090fe25
TA <thomas.el.alex@gmail.com>
2022-12-09 12:49:23 +0100

    interface skeleton

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from immoscraper import *
+from newservice import *
+from user import User
+import numpy
+import random
 
 #################################################
 # Ideas 
@@ -16,15 +20,17 @@ url = "https://www.immoweb.be/en/search/house/for-sale/leuven/3000?countries=BE&
 
 data_folder = "data"
 
-scrap = True
+scrap = False
 
+###########################################
+# Scrap data from IMMOWEB
 ###########################################
 
 # search for houses to buy in Leuven
 
 scrp = ImmoScraper(base_url=url)
 
-if (scrap):
+if (scrap):  # allows to load stored data
 
     scrp.get_web_data()  # ignores houses from ads "sponsored"
 
@@ -33,3 +39,56 @@ if (scrap):
 scrp.load_data(data_folder)
 
 scrp.print_data()
+
+avail_house_data = scrp.get_data_dict()
+house_keys = list(avail_house_data.keys())
+
+###########################################
+# run new service
+###########################################
+
+nS = NewService(avail_house_data)
+
+###########################################
+# Simulate user interface
+###########################################
+
+exit_flag = False
+curr_user_id = 0
+
+while (exit_flag==False):
+
+    # register new user in new service
+    input_house_id = int(input("\nPlease input property id (0 to %d):\t" % (len(avail_house_data))))
+
+    curr_user = User(id=curr_user_id, house_id_interest=house_keys[input_house_id])
+    nS.add_user(curr_user)
+    del curr_user
+
+    # loop for user to get proposals for different loan durations
+    exit_choice_flag = False
+    while(exit_choice_flag==False):
+
+        # invoke proposal calculation and visualization
+        answer = nS.prompt_choice(curr_user_id)
+
+        # ask for new choice or exit 
+        if (answer):
+            exit_choice_flag = True
+        else:
+            exit_choice_answer = input("\nPress 0 to continue, 1 to exit:\t")
+            if (exit_choice_answer):
+                exit_choice_flag = True
+
+    # ask for new user or exit
+    new_user_q = input("\nPress 1 for new user, or 0 to exit:\t") 
+    if (new_user_q):
+        curr_user_id += 1
+    else:
+        exit_flag = True
+
+# -------------------------------------------------
+
+print("\n\n--------------------------------------------------")
+print("\nTERMINATION\n")
+print("--------------------------------------------------\n\n")
