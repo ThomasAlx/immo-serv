fe58f804adf01c2d2527d4d8ec0aa8fc1710bf57
TA <thomas.el.alex@gmail.com>
2022-12-09 13:59:04 +0100

    adding user class

--- user.py
+++ user.py
@@ -2,8 +2,12 @@ import reader
 
 class User:
 
-    def __init__(self):
-        pass
+    def __init__(self, id=None, house_id_interest=None):
+        self.id = id
+        self.house_id_interest = house_id_interest
+        self.chose_loan_flag = None
+        self.choices = []
+        self.answers = []
 
     def set_user(self):
         # should use reader to save data in json ? files
