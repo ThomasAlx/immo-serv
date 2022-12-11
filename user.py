842e02484e85ec5a16e8a2da949c6435f58476c6
TA <thomas.el.alex@gmail.com>
2022-12-11 20:19:17 +0100

    rmv attributes

--- user.py
+++ user.py
@@ -5,9 +5,6 @@ class User:
     def __init__(self, id=None, house_id_interest=None):
         self.id = id
         self.house_id_interest = house_id_interest
-        self.chose_loan_flag = None
-        self.choices = []
-        self.answers = []
 
     def set_user(self):
         # should use reader to save data
